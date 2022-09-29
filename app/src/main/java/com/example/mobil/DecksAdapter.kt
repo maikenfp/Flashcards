6e926c6e0d2068a1742d164d1395dd8f48f7252a
Maiken Persen <maikenfridenp@hotmail.com>
2022-09-29 19:43:23 +0200

    Funksjonalitet for å klikkbar recyclerview items

--- app/src/main/java/com/example/mobil/DecksAdapter.kt
+++ app/src/main/java/com/example/mobil/DecksAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.mobil
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,32 +10,49 @@ import androidx.recyclerview.widget.RecyclerView
 
 class DecksAdapter(val context: Context, private val myList : ArrayList<String>) : RecyclerView.Adapter<DecksAdapter.ViewHolder>() {
 
+    lateinit var listener : onItemClickListener
+
+    interface onItemClickListener{
+
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener){
+
+        this.listener = listener
+
+    }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ViewHolder {
         val myDecksItem = LayoutInflater.from(viewGroup.context).inflate(R.layout.deck_item, viewGroup, false)
 
-        return ViewHolder(myDecksItem)
+        return ViewHolder(myDecksItem, listener)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val textItem = viewHolder.itemView.findViewById<TextView>(R.id.txt)
         val newList = myList[position]
 
-        textItem.text = newList
-
-        //viewHolder.bind(myList[position])
+        viewHolder.bind(newList)
     }
 
     override fun getItemCount(): Int {
         return myList.size
     }
 
-    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView : View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView) {
 
-        //private val textItem : TextView = itemView.findViewById(R.id.txt)
+        private val textItem = itemView.findViewById<TextView>(R.id.txt)
 
-        /*fun bind(deckItem : String){
+        fun bind(deckItem : String){
             textItem.text = deckItem
-        }*/
+        }
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
+
     }
 
 
--- app/src/main/java/com/example/mobil/MainActivity.kt
+++ app/src/main/java/com/example/mobil/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.mobil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         val myList = ArrayList<String>()
         myList.add("Mobile Programming")
@@ -38,25 +39,31 @@ class MainActivity : AppCompatActivity() {
 
             val addDialog = AlertDialog.Builder(this)
             addDialog.setView(inflater)
+
             addDialog.setPositiveButton("Ok"){
                     dialog,_->
                 val txt = addtxt.text.toString()
                 myList.add(txt)
                 myAdapter.notifyDataSetChanged()
-                //Toast.makeText(this,"Adding deck successful",Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
             }
+
             addDialog.setNegativeButton("Cancel"){
                     dialog,_->
                 dialog.dismiss()
-                //Toast.makeText(this,"Canceled",Toast.LENGTH_SHORT).show()
-
             }
+
             addDialog.create()
             addDialog.show()
+
         }
 
+        myAdapter.setOnItemClickListener(object : DecksAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                startActivity(Intent(this@MainActivity, Test::class.java))
+            }
 
+        })
 
     }
 }
