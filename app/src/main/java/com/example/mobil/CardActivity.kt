9662bb0db220ce335b8a9302153b1a3e3872dd13
KristofferLund94 <69959477+KristofferLund94@users.noreply.github.com>
2022-10-04 11:52:39 +0200

    cardactivity work

--- app/src/main/java/com/example/mobil/CardActivity.kt
+++ app/src/main/java/com/example/mobil/CardActivity.kt
@@ -3,7 +3,6 @@ package com.example.mobil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
 import com.example.mobil.model.Card
@@ -13,20 +12,14 @@ class CardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card)
+        setContentView(R.layout.activity_test)
 
         var index = 0
-        val cardText: TextView = findViewById(R.id.cardTextView)
+        val cardText = findViewById<TextView>(R.id.cardTextView)
 
         // Current Deck
         // ToDo: This ArrayList should be loaded with the correct deckId
         val cards = ArrayList<Card>()
-        cards.add(Card(0, "Card 0 Question", "Card 0 Answer", 0, false))
-        cards.add(Card(1, "Card 1 Question", "Card 1 Answer", 0, false))
-        cards.add(Card(2, "Card 2 Question", "Card 2 Answer", 0, false))
-        cards.add(Card(3, "Card 3 Question", "Card 3 Answer", 0, false))
-        cards.add(Card(4, "Card 4 Question", "Card 4 Answer", 0, false))
-
 
         // Go back button
         val goBackBtn = findViewById<Button>(R.id.goBackButton)
@@ -37,10 +30,9 @@ class CardActivity : AppCompatActivity() {
 
         // Hamburger Menu button
         // ToDo: Make Hamburger Button correctly
-        val editBtn = findViewById<Button>(R.id.editModeButton)
-        editBtn.setOnClickListener{
-            val inflater = LayoutInflater.from(this).inflate(R.layout.add_card,null)
-        }
+        //val hamburgerBtn = findViewById<Button>(R.id.editModeButton)
+        //hamburgerBtn.setOnClickListener{
+        //}
 
         // Previous Card button
         val previousCardBtn = findViewById<Button>(R.id.previousCardButton)
