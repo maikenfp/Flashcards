2cb8b8b7292089a3653aa96c5aa359b1828ee1b3
KristofferLund94 <69959477+KristofferLund94@users.noreply.github.com>
2022-10-05 09:54:01 +0200

    Fixed Card View
    - Moved Card View navigation buttons into the fragment
    - Removed Card View button icons, since they caused a crash
    - Tidied up some code surrounding that

--- app/src/main/java/com/example/mobil/CardFragment.kt
+++ app/src/main/java/com/example/mobil/CardFragment.kt
@@ -5,27 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.mobil.model.Card
 
-// TODO: Rename parameter arguments, choose names that match
+// TODO: Use parameters?
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var index = 0
+    private var cards = ArrayList<Card>()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            //ToDo: load deck and choose starting index
+            //int = it.getString(ARG_PARAM1)
+            //cards = it.getString(ARG_PARAM2)
         }
     }
 
@@ -34,8 +35,60 @@ class CardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false)
+        val view = inflater.inflate(R.layout.fragment_card, container, false)
+
+        //ToDo: Load deck instead of creating cards here
+        cards.add(Card(0, "Card 0 Question", "Card 0 Answer", 0, false))
+        cards.add(Card(1, "Card 1 Question", "Card 1 Answer", 0, false))
+        cards.add(Card(2, "Card 2 Question", "Card 2 Answer", 0, false))
+        cards.add(Card(3, "Card 3 Question", "Card 3 Answer", 0, false))
+        cards.add(Card(4, "Card 4 Question", "Card 4 Answer", 0, false))
+
+        val cardText = view.findViewById<TextView>(R.id.cardTextView)
+        cardText.setText(cards[index].question)
+
+
+        // Previous Card button
+        val previousCardBtn = view.findViewById<Button>(R.id.previousCardButton)
+        previousCardBtn.setOnClickListener{
+            if (index == 0) {
+                index = cards.size -1
+            }
+            else {
+                index -= 1
+            }
+            cardText.setText(cards[index].question)
+
+        }
+
+
+        // Next Card Button
+        val nextCardBtn = view.findViewById<Button>(R.id.nextCardButton)
+        nextCardBtn.setOnClickListener{
+            if (index == cards.size -1) {
+                index = 0
+            }
+            else {
+                index += 1
+            }
+            cardText.setText(cards[index].question)
+        }
+
+        // Flip Card Button
+        val flipCardBtn = view.findViewById<Button>(R.id.flipCardButton)
+        flipCardBtn.setOnClickListener{
+            if (cardText.text == cards[index].question) {
+                cardText.setText(cards[index].answer)
+            }
+            else {
+                cardText.setText(cards[index].question)
+            }
+        }
+        return view
     }
+
+
+
 
     companion object {
         /**
