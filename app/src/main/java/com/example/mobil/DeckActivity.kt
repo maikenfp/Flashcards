8ea6fbce84523444888768f5cbc22c742df19df2
Paige Traviata <88253079+Paigetraviata@users.noreply.github.com>
2022-10-06 19:00:40 +0200

    Changes in DeckActivity
    - EditMode is now created as a fragment, rather than an own activity.
    -Action buttons and recyclerview is now in their own fragments to be able to seperate functionality and views, while still being connected to the same activity.
    - Created layouts for add and delete card.(popups)
    -updated card item to fit with the fragments.
    - Added an ignore icon as vector asset.

--- app/src/main/java/com/example/mobil/DeckActivity.kt
+++ app/src/main/java/com/example/mobil/DeckActivity.kt
@@ -3,10 +3,8 @@ package com.example.mobil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobil.model.Card
@@ -17,67 +15,18 @@ class DeckActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deck)
 
-        val cards = ArrayList<Card>()
-        cards.add(Card(0, "Question?", "Answer!", 0, false))
-
-        // adapter & recycler
-        val cardAdapter = CardsAdapter(this, cards)
-        val cardRecycler = findViewById<RecyclerView>(R.id.card_recycle_view)
-
-        cardRecycler.adapter = cardAdapter
-        cardRecycler.layoutManager = LinearLayoutManager(this)
-
-        // Add button
-        val addBtn = findViewById<Button>(R.id.addCardButton)
-
-        addBtn.setOnClickListener {
-            val inflater = LayoutInflater.from(this).inflate(R.layout.add_card, null)
-            val addQuestion = inflater.findViewById<EditText>(R.id.input_question)
-            val addAnswer = inflater.findViewById<EditText>(R.id.input_answer)
-
-            val addCardDialog = AlertDialog.Builder(this)
-            addCardDialog.setView(inflater)
-
-            addCardDialog.setPositiveButton("Save") {
-                dialog,_->
-                val question = addQuestion.text.toString()
-                val answer = addAnswer.text.toString()
-                cards.add(Card(1, question, answer, 1, false))
-                cardAdapter.notifyDataSetChanged()
-                dialog.dismiss()
-            }
-
-            addCardDialog.setNegativeButton("Cancel") {
-                dialog,_->
-                dialog.dismiss()
-            }
-        }
-
-        // Edit button
-        val editBtn = findViewById<Button>(R.id.editModeButton)
-        editBtn.setOnClickListener{
-            val inflater = LayoutInflater.from(this).inflate(R.layout.add_card,null)
-            val addtxt = inflater.findViewById<EditText>(R.id.addText)
-        }
-
-        // Shuffle button
-        val shuffleBtn = findViewById<Button>(R.id.shuffleButton)
-        shuffleBtn.setOnClickListener{
-            val inflater = LayoutInflater.from(this).inflate(R.layout.add_card,null)
-            val addtxt = inflater.findViewById<EditText>(R.id.addText)
-        }
-
         // Go back button
         val goBackBtn = findViewById<Button>(R.id.goBackButton)
         goBackBtn.setOnClickListener{
             startActivity(Intent(this@DeckActivity, MainActivity::class.java))
         }
+    }
 
-        // Go to card
-        cardAdapter.setOnCardClickListener(object : CardsAdapter.onCardClickListener{
-            override fun onCardClick(position: Int) {
-                startActivity(Intent(this@DeckActivity, CardActivity::class.java))
-            }
-        })
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.deck_frame_fragment, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/mobil/DeckFragment.kt
@@ -0,0 +1,128 @@
+package com.example.mobil
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobil.model.Card
+import kotlin.random.Random
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DeckFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DeckFragment : Fragment() {
+    private var cards = ArrayList<Card>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_deck, container, false)
+
+        // adapter & recycler
+        val cardsAdapter = CardsAdapter(context = DeckActivity(), cards)
+        val cardsRecycler = view.findViewById<RecyclerView>(R.id.CardsRecyclerView)
+        cardsRecycler.adapter = cardsAdapter
+        cardsRecycler.layoutManager = LinearLayoutManager(context)
+
+        //ToDo: Load deck instead of creating cards here
+        cards.add(Card(0, "Card 0 Question", "Card 0 Answer", 0, false))
+        cards.add(Card(1, "Card 1 Question", "Card 1 Answer", 0, false))
+        cards.add(Card(2, "Card 2 Question", "Card 2 Answer", 0, false))
+        cards.add(Card(3, "Card 3 Question", "Card 3 Answer", 0, false))
+        cards.add(Card(4, "Card 4 Question", "Card 4 Answer", 0, false))
+
+
+        // Add button
+        val addBtn = view.findViewById<Button>(R.id.addCardButton)
+        addBtn.setOnClickListener {
+            val inflater = LayoutInflater.from(context).inflate(R.layout.add_card, null)
+            val addQuestion = inflater.findViewById<EditText>(R.id.enter_question)
+            val addAnswer = inflater.findViewById<EditText>(R.id.enter_answer)
+
+            val addCardDialog = AlertDialog.Builder(context)
+            addCardDialog.setView(inflater)
+
+            addCardDialog.setPositiveButton("Save") {
+                    dialog,_->
+                val question = addQuestion.text.toString()
+                val answer = addAnswer.text.toString()
+                cards.add(Card(1, question, answer, 1, false))
+                cardsAdapter.notifyDataSetChanged()
+                dialog.dismiss()
+            }
+
+            addCardDialog.setNegativeButton("Cancel") {
+                    dialog,_->
+                dialog.dismiss()
+            }
+            addCardDialog.create()
+            addCardDialog.show()
+        }
+
+        // Shuffle button
+        val shuffleBtn = view.findViewById<Button>(R.id.shuffleButton)
+        shuffleBtn.setOnClickListener {
+            val shuffleIndex = Random.nextInt(cards.size)
+            val shuffleElement = cards[shuffleIndex]
+            val intent = Intent(this@DeckFragment.context, CardActivity::class.java)
+
+            intent.putExtra(shuffleElement.question, shuffleElement.answer)
+            startActivity(intent)
+        }
+
+
+        // Edit button
+        val editBtn = view.findViewById<Button>(R.id.editModeButton)
+        editBtn.setOnClickListener {
+            (activity as DeckActivity).replaceFragment(EditFragment())
+        }
+
+        // Go to card
+        cardsAdapter.setOnCardClickListener(object : CardsAdapter.onCardClickListener{
+            override fun onCardClick(position: Int) {
+                startActivity(Intent(this@DeckFragment.context, CardActivity::class.java))
+            }
+        })
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment DeckFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DeckFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
