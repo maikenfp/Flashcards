1a1a716c3e412636adb65b38a9e01c15eff32625
Maiken Persen <maikenfridenp@hotmail.com>
2022-11-04 14:48:33 +0100

    Removed use of unessecary arraylist in decksadapter Cleaned some code in mainfragment

--- app/src/main/java/com/example/mobil/MainFragment.kt
+++ app/src/main/java/com/example/mobil/MainFragment.kt
@@ -37,19 +37,7 @@ class MainFragment : Fragment() {
     private var database : FirebaseFirestore = FirebaseFirestore.getInstance()
 
     val query : Query = database.collection("Decks")
-
-    // adapter & recycler
-    val decksAdapter = DecksAdapter(context = MainActivity(), decks, query)
-
-    override fun onResume() {
-        super.onResume()
-        decksAdapter.startListening()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        decksAdapter.stopListening()
-    }
+    val decksAdapter = DecksAdapter(context = MainActivity(), query)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -69,35 +57,11 @@ class MainFragment : Fragment() {
         //Add Deck
         val addDeckBtn = view.findViewById<Button>(R.id.addDeckButton)
         addDeckBtn.setOnClickListener {
-            val inflater = LayoutInflater.from(context).inflate(R.layout.add_deck, null)
-            val addTxt = inflater.findViewById<EditText>(R.id.addDeckName)
-
-            val addDeckDialog = AlertDialog.Builder(context)
-            addDeckDialog.setView(inflater)
-
-            addDeckDialog.setPositiveButton("Ok") {
-                    dialog,_->
-                val deckName = addTxt.text.toString()
-                val deck = hashMapOf(
-                    "title" to deckName
-                    //"cards" to cardList
-                )
-
-                database.collection("Decks").add(deck)
-                decksAdapter.notifyDataSetChanged()
-                dialog.dismiss()
-            }
-            addDeckDialog.setNegativeButton("Cancel") {
-                    dialog,_->
-                dialog.dismiss()
-            }
-            addDeckDialog.create()
-            addDeckDialog.show()
+            addDeck()
         }
 
         decksAdapter.setOnItemClickListener(object : DecksAdapter.OnItemClickListener {
             override fun onItemClick(position: Int) {
-                //(activity as MainActivity).replaceFragment(DeckFragment())
                 val currentTitle = decks[position].title.toString()
                 val currentId = database.collection("Decks").document(decks[position].docId.toString()).id
                 (activity as MainActivity).navigateToFragment("toCards", currentId, currentTitle)
@@ -106,6 +70,33 @@ class MainFragment : Fragment() {
                 Log.e("NAVIGATE TO TITLE: ", currentTitle)
             }
         })
+    }
+
+    private fun addDeck() {
+        val inflater = LayoutInflater.from(context).inflate(R.layout.add_deck, null)
+        val addTxt = inflater.findViewById<EditText>(R.id.addDeckName)
+
+        val addDeckDialog = AlertDialog.Builder(context)
+        addDeckDialog.setView(inflater)
+
+        addDeckDialog.setPositiveButton("Ok") {
+                dialog,_->
+            val deckName = addTxt.text.toString()
+            val deck = hashMapOf(
+                "title" to deckName
+                //"cards" to cardList
+            )
+
+            database.collection("Decks").add(deck)
+            decksAdapter.notifyDataSetChanged()
+            dialog.dismiss()
+        }
+        addDeckDialog.setNegativeButton("Cancel") {
+                dialog,_->
+            dialog.dismiss()
+        }
+        addDeckDialog.create()
+        addDeckDialog.show()
     }
 
     private fun eventChangeListener(adapter: DecksAdapter) {
@@ -130,6 +121,16 @@ class MainFragment : Fragment() {
                 adapter.notifyDataSetChanged()
             }
         })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        decksAdapter.startListening()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        decksAdapter.stopListening()
     }
 
     companion object {
