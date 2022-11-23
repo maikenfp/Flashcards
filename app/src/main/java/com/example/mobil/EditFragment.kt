7b4c666c5839cfbe2b3a962d98d45340ce1dad6a
Paige Traviata <88253079+Paigetraviata@users.noreply.github.com>
2022-11-23 14:16:41 +0100

    Removed unused code

--- app/src/main/java/com/example/mobil/EditFragment.kt
+++ app/src/main/java/com/example/mobil/EditFragment.kt
@@ -14,17 +14,6 @@ import com.example.mobil.databinding.FragmentEditBinding
 import com.example.mobil.model.Card
 import com.google.firebase.firestore.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-
 class EditFragment : Fragment() {
 
     // NAV ARGS
@@ -141,25 +130,4 @@ class EditFragment : Fragment() {
             }
         })
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
 }
--- app/src/main/java/com/example/mobil/MainFragment.kt
+++ app/src/main/java/com/example/mobil/MainFragment.kt
@@ -21,17 +21,6 @@ import com.google.firebase.firestore.*
 import com.google.firebase.firestore.ktx.firestoreSettings
 import java.lang.Error
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-
 class MainFragment : Fragment() {
 
     private var firebaseAuth : FirebaseAuth = FirebaseAuth.getInstance()
@@ -166,25 +155,5 @@ class MainFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         decksAdapter.stopListening()
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
